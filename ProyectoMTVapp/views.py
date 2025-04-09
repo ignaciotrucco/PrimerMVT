89c97534c6c537a1d7a47458536c46c90aa1fc77
ignaciotrucco <truccoignacio9@gmail.com>
2025-04-09 00:00:41 -0300

    modelo vista y coontrolador

--- ProyectoMTVapp/views.py
+++ ProyectoMTVapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Familiares
 
 # Create your views here.
+def lista_familiares(request):
+    lista_familiares = Familiares.objects.all()
+    return render(request, 'ProyectoMTVapp/lista_familiares.html', context={"familiares": lista_familiares})
--- ProyectoMVT/urls.py
+++ ProyectoMVT/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from ProyectoMTVapp import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ProyectoMTVapp/', views.lista_familiares, name='lista_familiares'),
 ]
